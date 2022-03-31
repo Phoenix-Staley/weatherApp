7e48b952d0b6138e2287be240057dea6458475ad
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-03-30 19:57:14 -0700

    Readded IP fetch

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -212,13 +212,13 @@ function loadPage() {
     loadRecents();
 
     // This fetches the user's current location, but doesn't work when hosted using HTTPS
-    // fetch("http://ip-api.com/json/")
-    //     .then((response) => {
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //         fetchWeatherData(data.city);
-    //     })
+    fetch("http://ip-api.com/json/", {mode: 'cors'})
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            fetchWeatherData(data.city);
+        })
 }
 
 loadPage();
